14e4ff64a0d80298141cc038b099dfe1471a8d92
Franco Vicente <francodvicente@outlook.com>
2024-03-23 22:05:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vacation and travel recommendations website project
+
+# The project revolves around showing the most touristic cities and others that, although they are not as visited, have very good reviews that guarantee that they are great vacation destinations.
+
+## Used technology
+
+- ### HTML
+- ### CSS
+- ### JS
+- ### Bootstrap
+- ### Git
---
+++ script.js
@@ -0,0 +1,13 @@
+function GoToSectionRecommendation() {
+    var elementos = document.getElementsByClassName("recommendations");
+    if (elementos.length > 0) {
+      elementos[0].scrollIntoView({ behavior: "smooth" });
+    }
+  }
+  function GoToSectionTourist() {
+    var elementos = document.getElementsByClassName("tourist-cities");
+    if (elementos.length > 0) {
+      elementos[0].scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
